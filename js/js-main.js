8f5950234cffa4c29e57b793c0854e343019bb22
alisa-pogarskaya <alisapogarskaya@gmail.com>
2018-04-26 19:27:08 +0300

    Страница "Контакты"
    Полностью готова страница "Контакты" и адаптирована.

--- js/js-main.js
+++ js/js-main.js
@@ -1,6 +1,10 @@
+// -- Topmenu-mobile -- //
+
 $('.nav-toggle').on('click', function(){
 		$('#topmenu').toggleClass('active');
 		});
+
+// -- Topmenu -- //
 		
 $(document).ready(function () {
 			  $('.sub > a').click(function(){
@@ -21,3 +25,30 @@ $(document).ready(function () {
 				 $(this).addClass('active');
 				});
 });
+
+
+// -- Map-address -- //
+
+function initMap() {
+		  
+	var map;
+
+	var myOptions = {
+		zoom: 16,
+		center: new google.maps.LatLng(59.965051, 30.312705),
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		styles: [{"featureType": "landscape","elementType": "labels","stylers": [{"visibility": "off"}]},{"featureType": "transit","elementType": "labels","stylers": [{"visibility": "off"}]},{"featureType": "poi","elementType": "labels","stylers": [{"visibility": "off"}]},{"featureType": "water","elementType": "labels","stylers": [{"visibility": "off"}]},{"featureType": "road","elementType": "labels.icon","stylers": [{"visibility": "off"}]},{"stylers": [{"hue": "#00aaff"},{"saturation": -100},{"gamma": 2.15},{"lightness": 12}]},{"featureType": "road","elementType": "labels.text.fill","stylers": [{"visibility": "on"},{"lightness": 24}]},{"featureType": "road","elementType": "geometry","stylers": [{"lightness": 57}]}]
+	}
+	
+	map = new google.maps.Map(document.getElementById("map-contacts"),myOptions);
+
+	// Marker - style
+	  
+	var markerContacts = new google.maps.Marker({
+		position: {lat: 59.965051, lng: 30.312705},
+		map: map,
+		title: 'Пункт',
+		icon: 'img/pin-map-office.svg'
+	});
+	
+}
